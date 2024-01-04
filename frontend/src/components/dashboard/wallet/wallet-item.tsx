303cebee0e4bd8b139f4891116bd4ac9b9a307ab
999bits <lewischeng21@gmail.com>
2024-01-03 21:48:03 -0600

    feat: added wallet items

--- frontend/src/components/dashboard/wallet/wallet-item.tsx
+++ frontend/src/components/dashboard/wallet/wallet-item.tsx
@@ -13,15 +13,15 @@ export const WalletItem: React.FC<WalletItemProps> = ({ wallet }: WalletItemProp
     return (
         <div className='flex flex-row p-[12px] items-center space-x-8 bg-neutral-900 border-[1px] border-zinc-800 rounded-md'>
             <div className='flex flex-col flex-1 space-y-2'>
-                <span className='text-sm font-bold'>{wallet.name}</span>
-                <span className='text-2xl font-bold'>${wallet.value}</span>
+                <span className='text-sm'>{wallet.name}</span>
+                <span className='text-2xl font-medium'>${wallet.value}</span>
             </div>
             <div className='flex flex-row flex-1'>
                 {wallet.tokens && Array.isArray(wallet.tokens) ? (
                     wallet.tokens.slice(0, 4).map((token: Token, index) => (
                         <Avatar className="w-[24px] h-[24px] -ml-1" key={index}>
                             <AvatarImage src={token.avatar} alt={token.name} />
-                            <AvatarFallback></AvatarFallback>
+                            <AvatarFallback className='bg-zinc-700'></AvatarFallback>
                         </Avatar>
                     ))
                 ) : (
